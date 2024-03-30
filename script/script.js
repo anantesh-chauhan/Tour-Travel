e607c561f6c76a0c7eb8ba3d7d8bcb7471f115d5
Aditya Maurya <adityamaurya1319@gmail.com>
2024-03-31 03:35:55 +0530

    review, brand ,footer and login

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 let searchBtn = document.querySelector('#search-btn');
-let searchBar = document.querySelector('.search-bar-containor');
+let searchBar = document.querySelector('.search-bar-container');
 let formBtn = document.querySelector('#login-btn');
-let loginForm = document.querySelector('.login-form-containor');
+let loginForm = document.querySelector('.login-form-container');
 let formClose = document.querySelector('#form-close');
 let menu =document.querySelector('#menu-bar');
 let navbar =document.querySelector('.navbar');
@@ -41,4 +41,45 @@ formClose.addEventListener('click', () =>{
         let src= btn.getAttribute('data-src');
         document.querySelector('#video-slider').src=src ;
     });
+ });
+ var swiper = new Swiper(".review-slider",{
+    spaceBetween: 20,
+    loop:true,
+    autoplay: {
+        delay:2500,
+        disableOnInteraction: false,
+    },
+    breakpoints:{
+        640: {
+            slidesPerView:1,
+        },
+        768: {
+            slidesPerView:2,
+        },
+        1024: {
+            slidesPerView:3,
+        },
+    }
+ });
+ var swiper = new Swiper(".brand-slider",{
+    spaceBetween: 20,
+    loop:true,
+    autoplay: {
+        delay:2500,
+        disableOnInteraction: false,
+    },
+    breakpoints:{
+        450: {
+            slidesPerView:2,
+        },
+        768: {
+            slidesPerView:3,
+        },
+        991: {
+            slidesPerView:4,
+        },
+        1200: {
+            slidesPerView:5,
+        },
+    }
  });
